a3fa780f54a1f2c498d9661722b97f04224925ee
anushabobbala <anushabobbala@gmail.com>
2016-11-22 20:34:29 +0530

    formated chair program

--- src/com/mylabs/Chair.java
+++ src/com/mylabs/Chair.java
@@ -1,23 +1,23 @@
 package com.mylabs;
-class Chair
-{
-String colour;
-int legs;
-Chair()
-{
-colour="blue";
-legs=4;
-}
-void print()
-{
-System.out.println(colour);
-System.out.println(legs);
-}
-public static void main(String []args)
-{
-Chair c1=new Chair();
-Chair c2=new Chair();
-c1.print();
-c2.print();
-}
+
+class Chair {
+	String colour;
+	int legs;
+
+	Chair() {
+		colour = "blue";
+		legs = 4;
+	}
+
+	void print() {
+		System.out.println(colour);
+		System.out.println(legs);
+	}
+
+	public static void main(String[] args) {
+		Chair c1 = new Chair();
+		Chair c2 = new Chair();
+		c1.print();
+		c2.print();
+	}
 }
