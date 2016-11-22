1ef818b431a84860d6920afb39f59bb0120f304c
anushabobbala <anushabobbala@gmail.com>
2016-11-22 22:35:21 +0530

    SimpleIfElseStatement program

---
+++ src/com/mylabs/SimpleIfElseStatementExample.java
@@ -0,0 +1,14 @@
+package com.mylabs;
+
+public class SimpleIfElseStatementExample {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+
+		if (i == 0)
+			System.out.println("i is 0");
+		else
+			System.out.println("i is not 0");
+	}
+}
