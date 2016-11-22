ebb59b9b27ba1d35d8e9d3f24f97d9daa197cfcb
anushabobbala <anushabobbala@gmail.com>
2016-11-22 21:57:08 +0530

    substring program

---
+++ src/com/mylabs/JavaSubstringExample.java
@@ -0,0 +1,15 @@
+package com.mylabs;
+
+public class JavaSubstringExample {
+
+	public static void main(String args[]) {
+
+		String name = "Hello World";
+
+		System.out.println(name.substring(6));
+
+		System.out.println(name.substring(0, 5));
+
+	}
+
+}
