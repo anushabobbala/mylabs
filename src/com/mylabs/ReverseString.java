2b4d2802d5126448a2c3276c940f2fbf4bd328bb
anushabobbala <anushabobbala@gmail.com>
2016-11-23 20:08:22 +0530

    reversestring program

---
+++ src/com/mylabs/ReverseString.java
@@ -0,0 +1,12 @@
+package com.mylabs;
+
+import java.lang.*;
+
+public class ReverseString {
+	public static void main(String args[]) {
+		StringBuilder sb = new StringBuilder("anu avi padma venkatrao");
+		System.out.println("string = " + sb);
+		System.out.println("reverse = " + sb.reverse());
+	}
+
+}
