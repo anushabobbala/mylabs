5d2284276dc2a30b7d3c73f58ece82f590e72a46
anushabobbala <anushabobbala@gmail.com>
2016-11-23 22:26:22 +0530

    added generics to revesestring

--- src/com/mylabs/ReverseStringArray.java
+++ src/com/mylabs/ReverseStringArray.java
@@ -1,15 +1,17 @@
 package com.mylabs;
 
-import java.lang.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ReverseStringArray {
 	public static void main(String[] args) {
-		List str = new ArrayList();
+		List<String> str = new ArrayList<>();
 		str.add("anu");
 		str.add("avi");
 		str.add("padma");
 		str.add("venkat");
+		
 		System.out.println("givenstring = " + str);
 		Collections.reverse(str);
 		System.out.println("reversed string = " + str);
