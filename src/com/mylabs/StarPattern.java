8700004f00b8971a213a91e734a9795cd33b0dc1
anushabobbala <anushabobbala@gmail.com>
2016-12-01 22:22:38 +0530

    display starPattern program

---
+++ src/com/mylabs/StarPattern.java
@@ -0,0 +1,15 @@
+package com.mylabs;
+
+public class StarPattern {
+	public static void main(String[] args){
+		int n=20; 
+		for (int i = 1; i <= n; i++) {
+		    for (int j = 1; j <= i; j++) {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+
+	}
+
+}
