eb1c32e29938542c0cec83fdaac865f9b5fff181
anushabobbala <anushabobbala@gmail.com>
2016-12-02 08:00:57 +0530

    PrintStars2 pattern program

---
+++ src/com/mylabs/PrintStars2.java
@@ -0,0 +1,20 @@
+package com.mylabs;
+
+public class PrintStars2 {
+	public static void print()
+	{
+	int count=7;
+	for (int i=0; i<count; i++) {
+		for( int j=0; j<=i; j++){
+		
+			System.out.print("*");
+		}
+	System.out.println();
+}
+	}	
+	public static void main(String[] args){
+		
+		  print();
+		
+	}
+}
