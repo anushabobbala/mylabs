840c4495e6fedaa0d8ab4e103f6d2fd6048776d2
anushabobbala <anushabobbala@gmail.com>
2016-12-05 19:49:15 +0530

    PrintNumbers program

---
+++ src/com/mylabs/PrintNumbers.java
@@ -0,0 +1,16 @@
+package com.mylabs;
+
+public class PrintNumbers {
+	public static void main(String[] args){
+		int count=3;
+		for(int i=0;i<=count;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print(i);
+			}
+			System.out.println(i);
+		}
+		
+		}
+	}
+
+
