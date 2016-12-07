6a88fb664c641e8b39483e97f5cf4e7676531f8b
Murali Padavala <mpadavala@gmail.com@>
2016-12-07 08:50:48 -0800

    modified to take parameter

--- src/com/mylabs/Print100Tables.java
+++ src/com/mylabs/Print100Tables.java
@@ -1,19 +1,22 @@
 package com.mylabs;
 
 public class Print100Tables {
-	public static void multiply() {
-		int i, j;
-		for (i = 1; i <= 100; i++) {
-			for (j = 1; j <= 20; j++) {
+	
+	public static void multiply(int numberOfTables) {
+
+		for (int i = 1; i <= numberOfTables; i++) {
+			
+			System.out.println("Multiplication Table for : " + i);
+			for (int j = 1; j <= 20; j++) {
 				System.out.println(i + "*" + j + "=" + i * j);
-
 			}
-
+			System.out.println("");
+			
 		}
 	}
 
 	public static void main(String[] args) {
-		multiply();
+		multiply(100);
 	}
 
 }
