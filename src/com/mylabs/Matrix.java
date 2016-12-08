8dddf059178e0f08ae5e4cc5910386f81545a080
Murali Padavala <mpadavala@gmail.com@>
2016-12-08 08:48:55 -0800

    matrix

---
+++ src/com/mylabs/Matrix.java
@@ -0,0 +1,37 @@
+package com.mylabs;
+
+public class Matrix {
+	
+	public static void main(String args[]){
+		int[][] matrix = createMatrix(10, 10);
+		printMatrix(matrix);
+	}
+	
+	public static int[][] createMatrix(int numRows, int numColumns){
+		
+		int[][] matrix = new int[numRows][numColumns];
+		
+		int number = 0;
+		for(int i=0; i<numRows; i++){
+			for(int j=0; j<numColumns; j++){
+				matrix[i][j] = number++;
+			}
+		}
+		return matrix;
+	}
+	
+	public static void printMatrix(int[][] matrix){
+		
+		int numRows = matrix.length;
+		int numColumns = matrix[0].length;
+		
+		for(int i=0; i<numRows; i++){
+			for(int j=0; j<numColumns; j++){
+				System.out.print(matrix[i][j]);
+				System.out.print("  ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
