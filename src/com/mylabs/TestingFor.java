0a288c1158efd9d0a0f22ed75db67cf0753d9118
anushabobbala <anushabobbala@gmail.com>
2016-12-23 21:35:39 +0530

    TestingFor program

---
+++ src/com/mylabs/TestingFor.java
@@ -0,0 +1,15 @@
+package com.mylabs;
+
+public class TestingFor {
+	public static void main(String[] args) {
+
+		int i = 22;
+		while (i < 20) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("error");
+
+	}
+
+}
