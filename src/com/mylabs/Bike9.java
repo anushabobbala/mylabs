ef16ad7062926f7f53380837f28b082218895402
anushabobbala <anushabobbala@gmail.com>
2017-02-09 18:18:48 +0530

    bike (final keyword example) program

---
+++ src/com/mylabs/Bike9.java
@@ -0,0 +1,12 @@
+package com.mylabs;
+class Bike9{  
+ final int speedlimit=90;//final variable  
+ 
+ void run(){  
+  speedlimit=400;  
+ }  
+ public static void main(String args[]){  
+ Bike9 obj=new  Bike9();  
+ obj.run();  
+ }  
+}//end of class  
