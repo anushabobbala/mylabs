ef843266958e8b411b4a38ce753c44c200c28075
anushabobbala <anushabobbala@gmail.com>
2017-02-09 18:21:11 +0530

    changed bike program

--- src/com/mylabs/Bike9.java
+++ src/com/mylabs/Bike9.java
@@ -1,9 +1,9 @@
 package com.mylabs;
 class Bike9{  
  final int speedlimit=90;//final variable  
- 
+  
  void run(){  
-  speedlimit=400;  
+  speedlimit++;  
  }  
  public static void main(String args[]){  
  Bike9 obj=new  Bike9();  
